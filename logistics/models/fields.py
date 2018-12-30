9de79cb5a2855e6ba09c11aa0407e8d3ffc67a38
jumshudsultan <cumshud@gmail.com>
2018-12-30 11:07:52 +0400

    POL field added

--- logistics/models/fields.py
+++ logistics/models/fields.py
@@ -1,31 +1,4 @@
-< ?xml
-version = '1.0'
-encoding = 'UTF-8'? >
-< odoo >
-< record
-id = "logistics_pol"
-model = "ir.model.fields"
-< field
-name = "field_description" > POL < / field >
-< field
-name = "model" > sale.order < / field >
-< field
-name = "model_id"
-ref = "sale.model_sale_order" / >
-< field
-name = "name" > logistics_pol < / field >
-< field
-name = "relation" > logistics.city < / field >
-< field
-name = "selectable"
-eval = "True" / >
-< field
-name = "state" > manual < / field >
-< field
-name = "store"
-eval = "True" / >
-< field
-name = "ttype" > many2one < / field >
-< / record >
+class SaleOrderInherited(models.Model) :
+    _inherit = 'sale.order'
 
-< / odoo >
+    custom_field = fields.Char(string='Custom File')
