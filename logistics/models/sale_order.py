7591c8b6c5c24c728acd6f8915526fa62d9e97e4
jumshudsultan <cumshud@gmail.com>
2018-12-31 22:47:50 +0400

    po/pod updated

--- logistics/models/sale_order.py
+++ logistics/models/sale_order.py
@@ -4,6 +4,7 @@ from odoo import models, fields, api
 class SaleCity(models.Model):
     _inherit = 'sale.order'
 
-    city_name = fields.Many2one('logistics.city', string="City Name")
+    city_name_pol = fields.Many2one('logistics.city', string="City Name")
+    city_name_pod = fields.Many2one('logistics.city', string="City Name")
     shipper = fields.Many2one('logistics.shipper', string="Shipper")
     shipper_contact = fields.Char(string='Shipper Contact', related=logistics.shipper.contact)
